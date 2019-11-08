ccfebf7eed227c2a35d9bdb65691348f67af55e6
deep <deep.dc216@gmail.com>
2019-11-08 15:58:09 +0530

    build

---
+++ build/precache-manifest.b71fba88924071a0312f7efe371b3f59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7ddf74a8f58d6d58d962dc8068ab69",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3946bdb8745e64c9f55f",
+    "url": "./static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "8d21ab3b73bb7ea6ca3d",
+    "url": "./static/css/main.fe2a7da6.chunk.css"
+  },
+  {
+    "revision": "3946bdb8745e64c9f55f",
+    "url": "./static/js/2.12cd54e5.chunk.js"
+  },
+  {
+    "revision": "8d21ab3b73bb7ea6ca3d",
+    "url": "./static/js/main.3ee300d6.chunk.js"
+  },
+  {
+    "revision": "58f38f09245fe9348e8a",
+    "url": "./static/js/runtime-main.eb912731.js"
+  },
+  {
+    "revision": "ecb0c2ae369ba2a89d9a1ec2a1b3187b",
+    "url": "./static/media/AvenirLTStd-Book.ecb0c2ae.otf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
